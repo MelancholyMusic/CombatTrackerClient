34e9b3856495b7662728e9507102493096bc78ab
Michael Fiedel <fiedelmike@gmail.com>
2017-01-08 20:17:07 -0700

    Facebook, Google, and basic UI

---
+++ Assets/Scripts/UI_Login.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Login : MonoBehaviour
+{
+	public InputField emailInput;
+	public InputField passwordInput;
+
+	public void BtnPressedLogin()
+	{
+		StartCoroutine(doWebAPILogin());
+	}
+
+	private IEnumerator doWebAPILogin()
+	{
+		Debug.Log("Attempting to login...");
+
+		WWWForm loginParams = new WWWForm();
+		loginParams.AddField("email", emailInput.text);
+		loginParams.AddField("password", passwordInput.text);
+
+		WWW loginRequest = new WWW("https://combattrackerserver20170105013416.azurewebsites.net/Account/Login", loginParams);
+		yield return loginRequest;
+
+		if(!string.IsNullOrEmpty(loginRequest.error))
+		{
+			Debug.LogError("Login ERROR: " + loginRequest.error);
+		}
+		else
+		{
+			Debug.Log("Login Success: " + loginRequest.text);
+		}
+	}
+}
