5f6732f1904b747efc7ac5cf2d52f77e69af7463
Mark Gibson <deedlesndeedles@gmail.com>
2017-01-16 07:19:08 -0700

    CharacterMain (~90%)

---
+++ Assets/Scripts/Player/Components/PResistance.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class PResistance
+    {
+        public string Description;
+        public int Value;
+
+        public PResistance(string description, int value)
+        {
+            Description = description;
+            Value = value;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Debug/CharacterTemp.cs
@@ -0,0 +1,14 @@
+﻿using Player;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterTemp
+{
+    public string Name = "Vanlith";
+    public int HP_Current = 28, HP_Max = 28, AC = 15, CMD = 14, Fort = 3, Ref = 5, Will = 11, Init = 2, Perception = 8, Speed = 20;
+    public List<PResistance> Resistances = new List<PResistance>() { new PResistance("DR / -", 5), new PResistance("Fire resistance", 10)};
+    public List<PStatus> Statuses = new List<PStatus>() { new PStatus(), new PStatus(), new PStatus()};
+    public Image Portrait;
+}
