06b2307dc85cb08e7aa2b138dfaff1dd700b1753
Michael Fiedel <fiedelmike@gmail.com>
2017-01-16 18:27:57 -0700

    Remove googleplay and facebook for now + settings to make android build

--- Assets/Scripts/UI/ScaleWithXOffset.cs
+++ Assets/Scripts/UI/ScaleWithXOffset.cs
@@ -2,38 +2,38 @@
 
 public class ScaleWithXOffset : MonoBehaviour
 {
-    public RectTransform cardRect;
-    public RectTransform glow;
+	public RectTransform cardRect;
+	public RectTransform glow;
 	public float screenWidth = Screen.width;
 	public float maxScale = 1.0f;
 	public float minScale = 0.8f;
 
-	float height;
-	float width;
+	//float height;
+	//float width;
 	float scale;
-    float prevX;
+	float prevX;
 
-    void Awake()
-    {
-        prevX = cardRect.position.x - 1;
-        height = cardRect.rect.height;
-        width = cardRect.rect.width;
-        scale = maxScale;
-        cardRect.localScale = new Vector3(scale, scale);
-    }
+	void Awake()
+	{
+		prevX = cardRect.position.x - 1;
+		//height = cardRect.rect.height;
+		//width = cardRect.rect.width;
+		scale = maxScale;
+		cardRect.localScale = new Vector3(scale, scale);
+	}
 
-    void Update()
-    {
-        if (prevX != cardRect.position.x)
-        {            
-            scale = (maxScale - minScale) + minScale*Mathf.Clamp(1 - Mathf.Abs((prevX - screenWidth/2)/(screenWidth/2)), minScale, maxScale);
-            cardRect.localScale = new Vector3(scale, scale);
-            prevX = cardRect.position.x;
-        }
-    }
+	void Update()
+	{
+		if(prevX != cardRect.position.x)
+		{
+			scale = (maxScale - minScale) + minScale * Mathf.Clamp(1 - Mathf.Abs((prevX - screenWidth / 2) / (screenWidth / 2)), minScale, maxScale);
+			cardRect.localScale = new Vector3(scale, scale);
+			prevX = cardRect.position.x;
+		}
+	}
 
-    float GetOffset()
-    {
-        return Mathf.Abs(prevX - (screenWidth));
-    }
+	float GetOffset()
+	{
+		return Mathf.Abs(prevX - (screenWidth));
+	}
 }
