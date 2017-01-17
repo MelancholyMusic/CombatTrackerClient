6fa8501dde3847288cf00a18fda55bf12931bd80
deedlesndeedles <deedlesndeedles@gmail.com>
2017-01-17 14:50:57 -0700

    Character Select Manager started

---
+++ Assets/Scripts/Managers/UICharacterSelectionManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UICharacterSelectionManager : MonoBehaviour
+{
+    public RectTransform list;
+    public GameObject card;
+
+    List<CharacterTemp> characters = new List<CharacterTemp>() { new CharacterTemp(), new CharacterTemp() };
+
+    void Awake()
+    {
+        CreateCards();
+    }
+
+    private void CreateCards()
+    {
+        list.DetachChildren(); //Remove Editor Children
+
+        foreach (CharacterTemp resistance in characters)
+        {
+            var res = Instantiate(card, list);
+            //res.FindChild("Label").GetComponent<Text>().text = resistance.Description;
+            //res.FindChild("ResText").GetComponent<Text>().text = resistance.Value.ToString();
+        }
+    }
+
+
+}
