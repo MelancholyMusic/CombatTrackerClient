1d3f1bfc217271cb5f5465eee0cf8ac6d87f8bb9
Mark Gibson <deedlesndeedles@gmail.com>
2017-01-16 23:29:47 -0700

    ScaleWithXOffset Update

--- Assets/Scripts/UI/ScaleWithXOffset.cs
+++ Assets/Scripts/UI/ScaleWithXOffset.cs
@@ -1,39 +1,65 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI.Extensions;
 
 public class ScaleWithXOffset : MonoBehaviour
 {
-	public RectTransform cardRect;
+    public RectTransform cardRect;
 	public RectTransform glow;
-	public float screenWidth = Screen.width;
+    public RectTransform screen;
 	public float maxScale = 1.0f;
 	public float minScale = 0.8f;
 
-	//float height;
-	//float width;
+    float screenWidth;
 	float scale;
 	float prevX;
+    UnityEngine.UI.Image glowImage;
+    bool IsGlowing;
 
 	void Awake()
 	{
+        glowImage = glow.GetComponent<UnityEngine.UI.Image>();
+        SetGlow(0);
+
+        screenWidth = screen.rect.width / 2 - 50;
 		prevX = cardRect.position.x - 1;
-		//height = cardRect.rect.height;
-		//width = cardRect.rect.width;
 		scale = maxScale;
 		cardRect.localScale = new Vector3(scale, scale);
 	}
 
 	void Update()
 	{
-		if(prevX != cardRect.position.x)
+        if(prevX != cardRect.position.x)
 		{
 			scale = (maxScale - minScale) + minScale * Mathf.Clamp(1 - Mathf.Abs((prevX - screenWidth / 2) / (screenWidth / 2)), minScale, maxScale);
 			cardRect.localScale = new Vector3(scale, scale);
-			prevX = cardRect.position.x;
+			prevX = cardRect.position.x;            
 		}
+
+        if (scale > maxScale*.95)
+        {
+            IsGlowing = true;
+        }
+        else
+            IsGlowing = false;
+
+        Glow();
 	}
 
-	float GetOffset()
-	{
-		return Mathf.Abs(prevX - (screenWidth));
-	}
+    private void Glow()
+    {
+        if (IsGlowing)
+            SetGlow(1 * Time.deltaTime, true);
+        else
+            SetGlow(-3 * Time.deltaTime, true);
+    }
+
+    private void SetGlow(float alpha, bool relative = false)
+    {
+        if (!relative)
+            glowImage.color = new Color(glowImage.color.r, glowImage.color.g, glowImage.color.b, alpha);
+        else
+            glowImage.color = new Color(glowImage.color.r, glowImage.color.g, glowImage.color.b, Mathf.Clamp(glowImage.color.a + alpha, 0, 1));
+
+    }
 }
