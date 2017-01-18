5ecceaabad4b237d553d487d6b1098225f24243d
Michael Fiedel <fiedelmike@gmail.com>
2017-01-17 21:35:23 -0700

    Slight reorg of project files (Assets -> Art)

---
+++ Assets/Scripts/UI/Windows/UI_CharacterSelection.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_CharacterSelection : MonoBehaviour
+{
+	public RectTransform list;
+	public RectTransform card;
+
+	private List<CharacterTemp> characters = new List<CharacterTemp>() { new CharacterTemp(), new CharacterTemp() };
+
+	void Awake()
+	{
+		CreateCards();
+	}
+
+	private void CreateCards()
+	{
+		list.DetachChildren(); //Remove Editor Children
+
+		foreach(CharacterTemp resistance in characters)
+		{
+			foreach(CharacterTemp character in characters)
+			{
+				//var res = Instantiate(card, list);
+				//res.FindChild("Label").GetComponent<Text>().text = resistance.Description;
+				//res.FindChild("ResText").GetComponent<Text>().text = resistance.Value.ToString();
+				var _card = Instantiate(card, list);
+				_card.FindChild("CharName").GetComponent<Text>().text = character.Name;
+				_card.FindChild("Campaign").GetComponent<Text>().text = character.Campaign;
+				_card.FindChild("ClassLevels").GetComponent<Text>().text = character.ClassLevels;
+			}
+		}
+	}
+}
